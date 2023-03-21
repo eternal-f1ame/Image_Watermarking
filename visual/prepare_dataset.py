edb3dfc62047e03573d3b458d2d2c34f77073439
eternal-f1ame <thunder.tanay.1907@gmail.com>
2023-03-22 03:33:28 +0530

    Update Visual Watermarking Dataset

--- visual/prepare_dataset.py
+++ visual/prepare_dataset.py
@@ -1,13 +1,14 @@
-import cv2
+"""Prepare Output Dataset for Visual Watermarking"""
 import os
-import numpy as np
+import cv2
 
-IMAGE_DIR ="/Users/ayushanand/programming/course_work/CV_project_1/Image_Watermarking/visual/coco128/images/train2017/"
+IMAGE_DIRS  = ["COCO_Dataset/", "100_Image_Dataset"]
 
-for img_name in os.listdir(IMAGE_DIR):
-    img = cv2.imread(os.path.join(IMAGE_DIR, img_name))
-    img = cv2.resize(img, (300,300))
-    watermark = cv2.imread("Watermark.png")
-    watermark = cv2.resize(watermark, (300, 300))
-    final = cv2.addWeighted(img, 0.8, watermark, 0.2, 0)
-    cv2.imwrite(os.path.join("./outputs/",img_name), final)
+for IMAGE_DIR in IMAGE_DIRS:
+    for img_name in os.listdir(("../"+IMAGE_DIR)):
+        img = cv2.imread("../"+os.path.join(IMAGE_DIR, img_name))
+        img = cv2.resize(img, (300,300))
+        watermark = cv2.imread("Watermark.png")
+        watermark = cv2.resize(watermark, (300, 300))
+        final = cv2.addWeighted(img, 0.8, watermark, 0.2, 0)
+        cv2.imwrite(os.path.join(("outputs/"+IMAGE_DIR),img_name), final)
